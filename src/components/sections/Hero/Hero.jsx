dbeca3501ac1179b7cb1cc53ae3c7f7ae9277b22
Sergey <sirgroshev@yandex.ru>
2026-07-05 16:13:59 +0300

    Работаю над адаптивом

--- src/components/sections/Hero/Hero.jsx
+++ src/components/sections/Hero/Hero.jsx
@@ -1,5 +1,4 @@
 import styles from "./Hero.module.css";
-// import Button from "../../Button/Button";
 import avatar from "../../../assets/ava.png";
 import { useTheme } from "../../../context/ThemeContext";
 import ParticleBackground from "../../ParticleBackground";
@@ -8,27 +7,27 @@ export default function Hero() {
   const { theme } = useTheme();
   const particleColor = theme === "light" ? "#0e1116" : "#f8f4f1";
   return (
-    <section className={styles.hero}>
+    <section className={styles.heroSection}>
       <ParticleBackground color={particleColor} />
-      <div className={styles.heroInfoBlock}>
-        <h1 className="highlightText">Привет, я Сергей</h1>
-        <p className="hero-subtitle">Начинающий фронтенд-разработчик</p>
-        <p className="hero-description">
-          Создаю современные веб-интерфейсы. Мой код - не просто вёрстка, а
-          работающие SPA с роутингом, API и адаптивом.
-        </p>
-        <div className={styles.heroButtons}>
-          <a href="#projects" className={styles.btn}>
-            Смотреть проекты
-          </a>
-          <a href="#contacts" className={styles.btn}>
-            Связаться
-          </a>
-          {/* <Button>Смотреть проекты</Button> 
-          <Button>Связаться</Button> */}
+      <div className={styles.hero}>
+        <div className={styles.heroInfoBlock}>
+          <h1 className="highlightText">Привет, я Сергей</h1>
+          <p className="hero-subtitle">Начинающий фронтенд-разработчик</p>
+          <p className="hero-description">
+            Создаю современные веб-интерфейсы. Мой код - не просто вёрстка, а
+            работающие SPA с роутингом, API и адаптивом.
+          </p>
+          <div className={styles.heroButtons}>
+            <a href="#projects" className={styles.btn}>
+              Смотреть проекты
+            </a>
+            <a href="#contacts" className={styles.btn}>
+              Связаться
+            </a>
+          </div>
         </div>
+        <img src={avatar} alt="bob" className={styles.avatarImg} />
       </div>
-      <img src={avatar} alt="bob" className={styles.avatarImg} />
     </section>
   );
 }
